7dbf43d89b4fe757844f6ea6e888843ff3cc7919
Pritam Chauhan <rajputpritam619@gmail.com>
2023-04-21 22:18:11 +0530

    header section done

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,7 @@ const addEventOnElem = function(elem, type, callback){
 const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelectorAll("[data-nav-toggler]");
 console.log(navToggler);
+console.log(navToggler);
 const overlay = document.querySelector("[data-overlay]");
 
 const toggleNavbar = function(){
@@ -24,3 +25,28 @@ const toggleNavbar = function(){
 }
 
 addEventOnElem(navToggler, "click", toggleNavbar);
+
+// close navbar when click on any navbar links
+
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const closeNavbar = function(){
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+addEventOnElem(navLinks,"click", closeNavbar);
+
+// Header active when scroll down
+const header = document.querySelector("[data-header]");
+
+const headerActive = function(){
+    if(window.scrollY > 100){
+        header.classList.add("active");
+    }
+    else{
+        header.classList.remove("active");
+    }
+}
+
+addEventOnElem(window, "scroll", headerActive);
