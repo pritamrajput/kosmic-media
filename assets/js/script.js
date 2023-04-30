89a69f1e32eb609d9ff956327fcd9426b6ecebdd
Pritam Chauhan <rajputpritam619@gmail.com>
2023-04-30 13:06:56 +0530

    landing page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,12 @@ addEventOnElement([navToggleBtn, overlay],"click", toggleNavbar);
 const parallaxElements = document.querySelectorAll("[data-parallax]");
 window.addEventListener("mousemove", event=>{
     for(let i = 0, len = parallaxElements.length; i < len; i++){
-         const movementX = (event.clientX / window.innerWidth)*Number(parallaxElements[i].dataset.parallaxElements)
+         const movementX = (event.clientX / window.innerWidth)*Number(parallaxElements[i].dataset.parallaxSpeed);
+
+         const movementY = (event.clientY / window.innerHeight)*Number(parallaxElements[i].dataset.parallaxSpeed);
+
+         parallaxElements[i].animate({
+            transform: `translate(${movementX}px, ${movementY}px)`
+         }, {duration: 500, fill:"forwards"})
     }
 })
